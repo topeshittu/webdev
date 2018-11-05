b44e2fac6a3afe5ad63881f463f9f64bc37517eb
Tope Shittu <tope4life@gmail.com>
2018-11-05 07:00:20 +0000

    update db url

--- app.js
+++ app.js
@@ -16,8 +16,11 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index")
     
-//mongoose.connect("mongodb://localhost/yelp_camp_v12Deployed", { useNewUrlParser: true });
-mongoose.connect("mongodb://donjoex:success123@ds151943.mlab.com:51943/yelpcamp");
+console.log(process.env.DATABASEURL);    
+    
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
+//mongoose.connect("mongodb://donjoex:success123@ds151943.mlab.com:51943/yelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(flash());
 app.set("view engine", "ejs");
